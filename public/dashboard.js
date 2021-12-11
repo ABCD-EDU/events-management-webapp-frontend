f719b0619f5e2f252a5ad07230fefef9a1bcb988
Jp De los Trinos <anona19@protonmail.com>
2021-12-11 17:15:21 +0800

    fix: main nav responsiveness and minor scss styles

--- public/dashboard.js
+++ public/dashboard.js
@@ -1,2 +1,10 @@
-const subNav = document.getElementById('.sub-nav');
+// this function runs when the DOM is ready, i.e. when the document has been parsed
+document.addEventListener("DOMContentLoaded", function() {
+    const mainNavLinks = document.querySelector("#main-nav-links");
+    const burgerMenu = document.querySelector("#burger-menu");
 
+    burgerMenu.addEventListener("click", () => {
+        mainNavLinks.classList.toggle("active");
+        burgerMenu.classList.toggle("active");
+    })
+});
