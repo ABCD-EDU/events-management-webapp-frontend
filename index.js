6467ced7435b51581cca81d14fbb9fac640ea7fe
arevalolance <lancearevalo2000@gmail.com>
2021-12-13 19:07:55 +0800

    add event loader

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const changeLogText = () => {
     if (data.message) {
       document.getElementById("log").innerText = "Logout";
     } else {
-      console.log(data)
+      console.log(data);
       document.getElementById("log").innerText = "Login";
     }
   });
@@ -24,21 +24,16 @@ const insertCreateEvent = () => {
       document.getElementById("create-event").style.display = "none";
     }
   });
-}
-
-const init = () => {
-  changeLogText();
-  insertCreateEvent();
-}
+};
 
 /**
  * TODO: Consider adding a toast when user has logged out successfully
- * 
+ *
  * As of now it just changes the Logout text to Login
  */
 const attemptLogin = () => {
   fetchAsync("session/isLogged").then((data) => {
-    console.log(data.message)
+    console.log(data.message);
     if (data.message) {
       fetchAsync("session/logout").then((data) => {
         if (data.message) {
@@ -46,10 +41,104 @@ const attemptLogin = () => {
         }
       });
     } else {
-      window.location.href = "session/login"
+      window.location.href = "session/login";
     }
   });
-}
+};
+
+const getUserEvents = () => {
+  fetchAsync("session/isLogged")
+    .then((data) => {
+      if (data.message) {
+        fetchAsync("events/user-events").then((res) => {
+          const _items = res;
+          const subNav = document.getElementById("events");
+          subNav.innerHTML = "";
+          for (var i = 0; i < _items.length; i++) {
+            const {
+              event_name,
+              address,
+              date_start,
+              date_end,
+              description,
+              event_status,
+              category,
+            } = _items[i];
+            subNav.innerHTML += `
+      <section id="card">
+          <div id="info">
+              <h2 id="event-name">${event_name}</h2>
+              <h3 id="address">${address}</h3>
+              <h3 id="date">Starting Date: ${date_start}</h3>
+              <h3 id="time">Ending Date: ${date_end}</h3>
+              <p id="description">${description}</p>
+          </div>
+          <div id="footer">
+              <h3 id="status">STATUS: ${event_status}</h3>
+              <div id="buttons">
+                  <button id="join">JOIN</button>
+                  <button id="leave">LEAVE</button>
+              </div>
+          </div>
+      </section>
+      `;
+          }
+        });
+      }
+    })
+    .catch(console.log);
+};
+
+const getUpcomingEvents = () => {
+  fetchAsync("session/isLogged")
+    .then((data) => {
+      if (data.message) {
+        fetchAsync("events/upcoming-events").then((res) => {
+          const _items = res;
+          const subNav = document.getElementById("events");
+          subNav.innerHTML = "";
+          for (var i = 0; i < _items.length; i++) {
+            const {
+              event_name,
+              address,
+              date_start,
+              date_end,
+              description,
+              event_status,
+              category,
+            } = _items[i];
+            subNav.innerHTML += `
+      <section id="card">
+          <div id="info">
+              <h2 id="event-name">${event_name}</h2>
+              <h3 id="address">${address}</h3>
+              <h3 id="date">Starting Date: ${date_start}</h3>
+              <h3 id="time">Ending Date: ${date_end}</h3>
+              <p id="description">${description}</p>
+          </div>
+          <div id="footer">
+              <h3 id="status">STATUS: ${event_status}</h3>
+              <div id="buttons">
+                  <button id="join">JOIN</button>
+                  <button id="leave">LEAVE</button>
+              </div>
+          </div>
+      </section>
+      `;
+          }
+        });
+      }
+    })
+    .catch(console.log);
+};
+
+const init = () => {
+  changeLogText();
+  insertCreateEvent();
+  getUserEvents();
+};
 
 document.body.onload = init;
 document.getElementById("log").onclick = attemptLogin;
+document.getElementById("upcoming-events").onclick = getUpcomingEvents;
+document.getElementById("my-events").onclick = getUserEvents;
